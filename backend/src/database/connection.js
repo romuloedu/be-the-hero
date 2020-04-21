7c03c9511c2b2118be96b0c85fa4a087ef932554
Romulo Eduardo <romulo.eduardo@squarelabs.com.br>
2020-04-21 16:27:29 -0300

    - Criação do controller de inclusão e listagem de ongs.

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,10 +1,10 @@
 const knex = require("knex");
 const configuration = require("../../knexfile");
 
-const config = process.env.NODE_ENV === 'development'
-    ? configuration.development
-    : configuration.production
+// const config = process.env.NODE_ENV === 'development'
+//     ? configuration.development
+//     : configuration.production
 
-const connection = knex(config);
+const connection = knex(configuration.development);
 
 module.exports = connection;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
+const OngController = require("./controllers/OngController");
+
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-    res.json({ message: "Hello World!" });
-})
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
 
 module.exports = routes;
